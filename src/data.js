c30020c1abc70a7b1a17b0d80d5ce96965928a7d
Diadem Grace Arroz <diademgracearroz@gmail.com>
2023-04-22 23:37:37 +0800

    added

--- src/data.js
+++ src/data.js
@@ -3,5 +3,6 @@ function dataSet() {
     return [
         // example
         // {humidity: 8.32, pressure: 9.84, rain: true}
+        // {characteristic: loyal, quality: integrity, jojowain: true}
     ]
 }
